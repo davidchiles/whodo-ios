64e6e73db2b371867cd77e392f37c915f4d946fb
David Chiles <dwalterc@gmail.com>
2014-03-20 12:37:14 -0700

    Initial Commit

---
+++ whodo/WDOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WDOAppDelegate.h
+//  whodo
+//
+//  Created by David Chiles on 3/20/14.
+//  Copyright (c) 2014 davidchiels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WDOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ whodoTests/whodoTests.m
@@ -0,0 +1,34 @@
+//
+//  whodoTests.m
+//  whodoTests
+//
+//  Created by David Chiles on 3/20/14.
+//  Copyright (c) 2014 davidchiels. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface whodoTests : XCTestCase
+
+@end
+
+@implementation whodoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
